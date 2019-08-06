a8ca7adf34dd27151f09fe95a001fb091cec1506
Avira George <avira@bhagin.com>
2019-08-06 14:14:04 +0530

    Added name to site landing page

--- info.php
+++ info.php
@@ -78,7 +78,7 @@
 <div class="container-fluid bg-1 text-center">
   <h3 class="margin">Who Am I?</h3>
   <img src="https://www.w3schools.com/bootstrap/bird.jpg" class="img-responsive img-circle margin" style="display:inline" alt="Bird" width="350" height="350">
-  <h3>I'm an adventurer</h3>
+  <h3>I'm Avira George</h3>
 </div>
 
 <!-- Second Container -->
